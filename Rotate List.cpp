2b56060bf86f3064c33e616ec689d020134e0b1e
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2022-02-09 09:34:21 +0530

    Create Rotate List.cpp

---
+++ Rotate List.cpp
@@ -0,0 +1,23 @@
+// https://www.interviewbit.com/problems/rotate-list/
+
+
+
+ListNode* Solution::rotateRight(ListNode* head, int k) {
+    
+    if(head== NULL) return head;
+        ListNode *curr = head;
+        int n = 1;
+        while(curr->next){
+            curr = curr->next;
+            n++;
+        }
+        curr->next = head;
+        if(k > n) k = k%n;
+        
+        for(int i = 0; i < n-k; i++)
+            curr= curr->next;
+        
+        ListNode *ans = curr->next;
+        curr->next = NULL;
+        return ans;
+}
